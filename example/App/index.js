1e8fdfb0894a3da7d2a5f5fa0a4206aa4bb081b7
wwm <wwm@yizhilaomuzhu.local>
2017-03-17 12:51:13 +0800

    初始化ios版本

---
+++ example/App/index.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+import {
+    Dimensions,
+    ScrollView,
+    Text,
+    TouchableHighlight,
+    View,
+    ListView,
+    Image,
+} from 'react-native';
+
+import Calendar from "react-native-calendar-view"
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            // today:new Date(2015,1,2),
+            // min:new Date(2015,1,1),
+            // max:new Date(2015,1,28)
+            fillDefaultColorDates:{'2017-03-01':'#00ff00','2017-03-02':'#0000ff'}
+        };
+
+    }
+
+    static propTypes = {}
+
+    static defaultProps = {}
+
+    render() {
+        console.log("====render",this.state)
+        return (
+            <View  style={{
+            paddingTop:20,
+            alignSelf: 'center'
+        }}>
+           <Calendar
+            
+            height={280}
+            tileHeight={35}
+            hideHeader={false}
+            today={this.state.today}
+            selectedDates={this.state.dates}
+            dateBounds={[this.state.min, this.state.max]}
+            fillDefaultColorDates={this.state.fillDefaultColorDates}
+        
+            topbarVisible={true}
+            datesSelection={'single'}
+            firstDayOfWeek="monday"
+            showOtherDates="none"
+           
+            eventsColor="#9C27B0"
+            onSelectDate={date => {
+            console.log(date);
+        }}
+            onMonthChange={month => {
+            console.log(month);
+        }}/>
+        <Text>12312312x</Text>
+        </View>
+        )
+    }
+}
