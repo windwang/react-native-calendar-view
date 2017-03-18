e3949ca05fd76f424cec1d04070953504c3102f3
windwang <windwang@gmail.com>
2017-03-18 17:18:17 +0800

    整合MeterialCalendarView以方便扩展自定义视图

---
+++ android/src/main/java/com/prolificinteractive/materialcalendarview/CustomMonthPagerAdapter.java
@@ -0,0 +1,14 @@
+package com.prolificinteractive.materialcalendarview;
+
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.MonthPagerAdapter;
+import com.prolificinteractive.materialcalendarview.MonthView;
+
+/**
+ * Created by wwm on 2017-03-18.
+ */
+public class CustomMonthPagerAdapter extends MonthPagerAdapter {
+    CustomMonthPagerAdapter(MaterialCalendarView mcv) {
+        super(mcv);
+    }
+}
