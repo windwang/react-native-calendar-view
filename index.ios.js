17af0179203bb12811c6de1b3249a4eb2c9a17d3
windwang <windwang@gmail.com>
2017-03-21 17:34:44 +0800

    ios update

--- index.ios.js
+++ index.ios.js
@@ -1,27 +1,32 @@
-import React, { Component } from 'react'
-import { Dimensions, processColor, requireNativeComponent } from 'react-native'
+import React, {Component} from 'react'
+import {Dimensions, processColor, requireNativeComponent} from 'react-native'
 import _ from 'lodash'
 
 function convertProps(props) {
 
-
-  if(props.fillDefaultColorDates){
-    const items=props.fillDefaultColorDates;
-    props.fillDefaultColorDates={};
-    props.subtitleDefaultColorDates={};
-    props.subtitleForDates={};
-    _.each(items,(item,key)=>{         
-        props.fillDefaultColorDates[key]= processColor(item.color);
-        props.subtitleDefaultColorDates[key]=item.textColor;
-        props.subtitleForDates[key]=item.text;
-    });
+  if (props.fillDefaultColorDates) {
+    const items = props.fillDefaultColorDates;
+    props.fillDefaultColorDates = {};
+    props.subtitleDefaultColorDates = {};
+    props.subtitleForDates = {};
+    _.each(items, (item, key) => {
+      props.fillDefaultColorDates[key] = processColor(item.color);
+      if (item.textColor) 
+        props.subtitleDefaultColorDates[key] = item.textColor;
+      if (item.text) 
+        props.subtitleForDates[key] = item.text;
+      }
+    );
   }
 
-
   let allKeys = [
-    'fillSelectionColorDates', 'borderDefaultColorDates',
-    'borderSelectionColorDates', 'titleDefaultColorDates', 'titleSelectionColorDates',
-    'subtitleDefaultColorDates', 'subtitleSelectionColorDates'
+    'fillSelectionColorDates',
+    'borderDefaultColorDates',
+    'borderSelectionColorDates',
+    'titleDefaultColorDates',
+    'titleSelectionColorDates',
+    'subtitleDefaultColorDates',
+    'subtitleSelectionColorDates'
   ];
   _.each(allKeys, (key) => {
     if (!props[key]) {
@@ -32,7 +37,9 @@ function convertProps(props) {
     })
   })
   if (props.today) {
-    props.today = props.today.getTime()
+    props.today = props
+      .today
+      .getTime()
   }
   if (props.dateBounds) {
     _.each(props.dateBounds, (value, index) => {
@@ -54,15 +61,21 @@ export default class FSCalendar extends Component {
   static propTypes = {
     width: React.PropTypes.number,
     height: React.PropTypes.number,
-    hideHeader: React.PropTypes.bool,         // 是否隐藏头部
-    hideWeekDay: React.PropTypes.bool,        // 是否隐藏顶部的星期
+    hideHeader: React.PropTypes.bool, // 是否隐藏头部
+    hideWeekDay: React.PropTypes.bool, // 是否隐藏顶部的星期
     scrollEnabled: React.PropTypes.bool,
-    today: React.PropTypes.instanceOf(Date),
-    dateBounds: React.PropTypes.arrayOf(Date),
-    scopeMode: React.PropTypes.oneOf(['month', 'week']),
-    borderRadius:React.PropTypes.number,
+    today: React
+      .PropTypes
+      .instanceOf(Date),
+    dateBounds: React
+      .PropTypes
+      .arrayOf(Date),
+    scopeMode: React
+      .PropTypes
+      .oneOf(['month', 'week']),
+    borderRadius: React.PropTypes.number,
 
-    subtitleTextSize:React.PropTypes.number,
+    subtitleTextSize: React.PropTypes.number,
 
     // 回调函数
     onSelectDate: React.PropTypes.func,
@@ -98,14 +111,16 @@ export default class FSCalendar extends Component {
     fillSelectionColorDates: React.PropTypes.object,
     borderDefaultColorDates: React.PropTypes.object,
     borderSelectionColorDates: React.PropTypes.object,
-    cellShapeDates: React.PropTypes.objectOf(React.PropTypes.number),
+    cellShapeDates: React
+      .PropTypes
+      .objectOf(React.PropTypes.number),
 
     titleDefaultColorDates: React.PropTypes.object,
     titleSelectionColorDates: React.PropTypes.object,
     subtitleDefaultColorDates: React.PropTypes.object,
     subtitleSelectionColorDates: React.PropTypes.object,
 
-    subtitleForDates:React.PropTypes.object,
+    subtitleForDates: React.PropTypes.object,
 
     // 头部样式
     headerMinimumDissolvedAlpha: React.PropTypes.number,
@@ -113,15 +128,17 @@ export default class FSCalendar extends Component {
   }
 
   static defaultProps = {
-    width: Dimensions.get('window').width,
-    height: 280,            // scopeMode 为 month 建议高度 280, 为 week 建议高度 70
+    width: Dimensions
+      .get('window')
+      .width,
+    height: 280, // scopeMode 为 month 建议高度 280, 为 week 建议高度 70
     hideHeader: false,
     hideWeekDay: false,
     scrollEnabled: true,
     today: null,
     scopeMode: 'month',
-    borderRadius:0.15
-    
+    borderRadius: 0.15
+
   }
 
   componentWillReceiveProps(nextProps) {
@@ -129,34 +146,43 @@ export default class FSCalendar extends Component {
     convertProps(this._props)
   }
   _onSelectDate(ev) {
-    this.props.onSelectDate && this.props.onSelectDate(new Date(ev.nativeEvent.date))
+    this.props.onSelectDate && this
+      .props
+      .onSelectDate(new Date(ev.nativeEvent.date))
   }
   _onDeselectDate(ev) {
-    this.props.onDeselectDate && this.props.onDeselectDate(new Date(ev.nativeEvent.date))
+    this.props.onDeselectDate && this
+      .props
+      .onDeselectDate(new Date(ev.nativeEvent.date))
   }
   _onCurrentPageChange(ev) {
-    this.props.onMonthChange && this.props.onMonthChange(new Date(ev.nativeEvent.date))
+    this.props.onMonthChange && this
+      .props
+      .onMonthChange(new Date(ev.nativeEvent.date))
   }
 
   render() {
-    const { onSelectDate, onDeselectDate, onMonthChange, ...nativeProps } = this._props;
-    return (
-      <RNFSCalendar onSelectDate={(ev) => this._onSelectDate(ev)}
-        onDeselectDate={(ev) => this._onDeselectDate(ev)}
-        onCurrentPageChange={(ev) => this._onCurrentPageChange(ev)}
-        { ...nativeProps } />
-    )
+    const {
+      onSelectDate,
+      onDeselectDate,
+      onMonthChange,
+      ...nativeProps
+    } = this._props;
+    return (<RNFSCalendar
+      onSelectDate={(ev) => this._onSelectDate(ev)}
+      onDeselectDate={(ev) => this._onDeselectDate(ev)}
+      onCurrentPageChange={(ev) => this._onCurrentPageChange(ev)}
+      { ...nativeProps }/>)
   }
 }
 
 const RNFSCalendar = requireNativeComponent('RNFSCalendar', {
   propTypes: {
     ...FSCalendar.propTypes,
-    onCurrentPageChange:React.PropTypes.func,
+    onCurrentPageChange: React.PropTypes.func,
     today: React.PropTypes.number
   }
 })
-
 
 export const CellShape = {
   Circle: 0,
